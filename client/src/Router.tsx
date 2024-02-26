c371bf1bd6777b86b56900b3c111c79af8f4b144
Sean Flanagan <flanspan11@gmail.com>
2024-02-26 18:51:11 -0500

    feat: Creating empty components for header, footer, and aside

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,11 +1,21 @@
 import { HomePage } from "./pages/Home/Home.page";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Aside } from "./components/Aside";
 
 /*
   A router could go here but the app only has one page. 
   If there are multiple routers (e.g. authenticated vs unauthenticated users)
-  then I would recommend putting routers in a separate directory rather in 
+  then I would prefer putting routers in a separate directory rather in 
   the root of /src. 
 */
 export const Router = () => {
-  return <HomePage />;
+  return (
+    <div>
+      <Header />
+      <HomePage />
+      <Aside />
+      <Footer />
+    </div>
+  );
 };
