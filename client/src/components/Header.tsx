4dc320fc06b44cc92284293d13f8f4275a15a4e5
Sean Flanagan <flanspan11@gmail.com>
2024-02-26 19:39:17 -0500

    bug: fixing flex for main component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,9 +3,9 @@ import { LogoutSVG } from "../assets/svgs/LogoutSVG";
 
 export const Header = () => {
   return (
-    <header className="bg-blue-500 flex items-center">
+    <header className="bg-blue-500 flex items-center justify-between p-4">
       <GolfSVGLogo height={50} width={50} />
-      <h1>Heading</h1>
+      <h1 className="text-3xl">Heading</h1>
       <LogoutSVG height={24} width={24} />
     </header>
   );
--- client/src/pages/Home/Home.page.tsx
+++ client/src/pages/Home/Home.page.tsx
@@ -1,3 +1,3 @@
 export const HomePage = () => {
-  return <div className="bg-orange-500">Home</div>;
+  return <div className="bg-orange-500 flex-grow">Home</div>;
 };
