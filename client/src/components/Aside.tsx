35e58626a26edad8080774dc7ae22779d7d94888
Sean Flanagan <flanspan11@gmail.com>
2024-02-26 20:16:53 -0500

    feat: Setup for selecting a location from dropdown

--- client/src/components/Aside.tsx
+++ client/src/components/Aside.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export const Aside = () => {
-  return <aside className="bg-red-500">Aside</aside>;
+  const [selectedLocation, setSelectedLocation] = useState(0);
+
+  console.log("SelectedLocation", selectedLocation);
+
+  return (
+    <aside className="bg-red-500 min-w-40">
+      <select
+        name="selectedLocation"
+        id="selectedLocation"
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+          setSelectedLocation(Number(e.target?.value) || 0)
+        }
+      >
+        <option value={0}>Choose a location</option>
+        <option value={1}>Location1</option>
+        <option value={2}>Location2</option>
+        <option value={3}>Location3</option>
+        <option value={4}>Location4</option>
+      </select>
+    </aside>
+  );
 };
