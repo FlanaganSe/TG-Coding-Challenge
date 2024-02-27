d36374fd115c3957d88359d5ffbbeea5d2f455b6
Sean Flanagan <flanspan11@gmail.com>
2024-02-27 17:42:30 -0500

    cleanup: Cleaning up home contents

--- client/src/pages/Home/components/HomeContent.tsx
+++ client/src/pages/Home/components/HomeContent.tsx
@@ -1,14 +1,15 @@
 import { useLocations } from "../../../stores/locationStore";
 
 export const HomeContent = () => {
-  const location = useLocations().selectedLocation;
+  const { name, details, img, features } = useLocations().selectedLocation;
   if (!location) return <h1>Loading...</h1>;
   return (
     <div>
-      <h2>{location.details}</h2>
-      <h2>{JSON.stringify(location.features)}</h2>
-      <h2>{location.img}</h2>
-      <img src={location.img} alt={location.details} />
+      <h2>{img}</h2>
+      <h2>{name}</h2>
+      <h2>{details}</h2>
+      <h2>{features.amenities}</h2>
+      <img src={img} alt={details} />
     </div>
   );
 };
