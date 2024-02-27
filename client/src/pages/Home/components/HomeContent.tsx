3d310cc59ff6336fc286e902b4549504bf607e97
Sean Flanagan <flanspan11@gmail.com>
2024-02-27 18:03:35 -0500

    feat: improving location content shown in home contente component

--- client/src/pages/Home/components/HomeContent.tsx
+++ client/src/pages/Home/components/HomeContent.tsx
@@ -2,14 +2,40 @@ import { useLocations } from "../../../stores/locationStore";
 
 export const HomeContent = () => {
   const { name, details, img, features } = useLocations().selectedLocation;
-  if (!location) return <h1>Loading...</h1>;
+
+  if (!name) return <h1>No selected location...</h1>;
+
+  const { amenities, virtual_tour_link, floors } = features;
+
   return (
     <div>
-      <h2>{img}</h2>
-      <h2>{name}</h2>
-      <h2>{details}</h2>
-      <h2>{features.amenities}</h2>
       <img src={img} alt={details} />
+      <h1 className="text-3xl py-4">{name}</h1>
+      <div className="flex flex-row pt-6">
+        <div className="flex-1">
+          <h2>{details}</h2>
+        </div>
+        <div className="flex-1">
+          <p>Features:</p>
+          {/* The features object could changed to a list with Object.entries()
+          and mapped over to generate the list items but it's typically best to
+           avoid changingdata structures in the client */}
+          <ul>
+            {amenities && (
+              <li>Amenities: {amenities.join(", ").replaceAll("_", " ")}</li>
+            )}
+            {virtual_tour_link && (
+              <li>
+                Virtual Tour:{" "}
+                <a className="text-blue-500" href={virtual_tour_link}>
+                  Link!
+                </a>
+              </li>
+            )}
+            {floors && <li>Number of floors: {floors}</li>}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
