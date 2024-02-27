c06c51467d54d9dc43ed3021c984b09b0707c902
Sean Flanagan <flanspan11@gmail.com>
2024-02-27 17:48:18 -0500

    style: General style changes to bottom footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-green-500 p-2">
+    <footer className="pt-4 border-gray-500 border-t-2 border-dotted">
       <p>
         Just a fun coding take home. Check out the{" "}
         <a href="https://github.com/FlanaganSe/TG-Coding-Challenge">Github!</a>
