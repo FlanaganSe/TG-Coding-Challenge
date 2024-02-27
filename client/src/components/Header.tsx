80495f95c96a75c8133c010dbb74795dac11a112
Sean Flanagan <flanspan11@gmail.com>
2024-02-27 17:45:57 -0500

    feat: cleanup and improve responsiveness of heading

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,9 +3,13 @@ import { LogoutSVG } from "../assets/svgs/LogoutSVG";
 
 export const Header = () => {
   return (
-    <header className="bg-blue-500 flex items-center justify-between p-4">
-      <GolfSVGLogo />
-      <h1 className="text-3xl">Top Golf</h1>
+    <header className="flex items-center justify-between pb-4">
+      <a href="/">
+        <GolfSVGLogo className="h-8 md:h-14 transition hover:scale-110 duration-300" />
+      </a>
+      <h1 className="text-xl md:text-3xl [text-shadow:1px_1px_var(--tw-shadow-color)] shadow-red-200 italic">
+        TG: Where Excitement Begins
+      </h1>
       <LogoutSVG />
     </header>
   );
